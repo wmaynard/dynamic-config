120d637ef7f665106c801196fd7e56a41e42b1ac
Will Maynard <will@willmaynard.com>
2022-08-12 02:09:27 -0700

    Bump to common-1.1.55
    Adds registration name to platform options, better logging, update error codes

--- Startup.cs
+++ Startup.cs
@@ -11,8 +11,10 @@ public class Startup : PlatformStartup
 	protected override PlatformOptions Configure(PlatformOptions options) => options
 		.SetProjectOwner(Owner.Will)
 		.SetPerformanceThresholds(warnMS: 30_000, errorMS: 60_000, criticalMS: 90_000)
+		.SetRegistrationName("Dynamic Config")
 		.SetLogglyThrottleThreshold(suppressAfter: 10, period: 1_800)
+		.DisableServices(CommonService.Config)
 		// .DisableFeatures(CommonFeature.ConsoleColorPrinting)
-		// .DisableFeatures(CommonFeature.ConsoleObjectPrinting)
+		.DisableFeatures(CommonFeature.ConsoleObjectPrinting)
 	;
 }
